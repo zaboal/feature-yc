0179a224cb1dc07c763b5fe4b68c5ba1e2c0a8b5
Bogdan Zažigin <me@zba.su>
2025-02-21 03:20:19 +0000

    Download the script instead of commiting it into the repository

--- src/yc/install.sh
+++ src/yc/install.sh
@@ -3,13 +3,10 @@
 # Install for the user the dev container will be used with,
 # as for mcr.microsoft.com/devcontainers/base:*, where the user is not "root".
 # https://containers.dev/implementors/features/#user-env-var
-USERNAME=${USERNAME:-$_REMOTE_USER}
-if [ "$USERNAME" = "root" ]; then
-  HOME="/root"
-else
-  HOME="/home/$USERNAME"
-fi
 
 # Have to download this file at check for its actual options
 # before putting them instide of devcontainer-feature.json.
+
+su -c '
 curl -sSL https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
+' $_REMOTE_USER
